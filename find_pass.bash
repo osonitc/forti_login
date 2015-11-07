bdcbe5326a91f7d5075452a1d9288b366498e929
Guest <guest-GS93LZ@dl-101>
2015-11-07 22:25:35 +0530

    bash script to find users who have not changed their passwords

--- find_pass.bash
+++ find_pass.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
 touch nochange.list
+echo "create"
 for i in `cat roll.list`
 do
-	bash forti_login ${i} ${i} | grep "Logged in."
+	echo "start"
+	echo ${i}
+	bash forti_login ${i} "nitc1234" | grep "Logged in."
 	if [ $? -eq 0 ]; then
+		echo "s"
 		echo ${i} >> nochange.list
 	else
-		echo "trying roll number ${i}"
+		echo "f"
 	fi
+	echo "next"
 done
+echo "end"
